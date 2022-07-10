8b56a7e774de7fcd106a413fcf63dc98c651961b
ImDavide <98126828+ImDavide@users.noreply.github.com>
2022-07-10 20:55:05 +0200

    Update price-list.js

--- Commands/Utility/price-list.js
+++ Commands/Utility/price-list.js
@@ -12,7 +12,7 @@ module.exports = {
         const Embed = new MessageEmbed()
 
     .setTitle("DS Studios - Pricelist")
-    .setDescription("<a:955148947423379526:995329371101466695>**・Portfolio**\n\nLogos ➜ 6€\nGifs ➜ 6€\nThumbnail Fivem ➜ 5€\nBanner ➜ 4€\nWallpaper ➜ 3€\nOutro ➜ 5€\nDrawings ➜ 7€\n\n<a:955148947423379526:995329371101466695>**・Bundles**\n\nLogo + Gif ➜ 7€\nDrawing + Thumbnail ➜ 8€\n\n<a:955148947423379526:995329371101466695>**・Payment Methods**\n\n**PayPal**, **Nitro**, are the only accepted methods\n\n <:987612992294252615:995745604842033152> - OPEN A TICKET IN <#986311016755384320> AND BUY NOW! -  <:si:986356619602448464>")
+    .setDescription("<a:955148947423379526:995764754129887253>**・Portfolio**\n\nLogos ➜ 6€\nGifs ➜ 6€\nThumbnail Fivem ➜ 5€\nBanner ➜ 4€\nWallpaper ➜ 3€\nOutro ➜ 5€\nDrawings ➜ 7€\n\n<a:955148947423379526:995764754129887253>**・Bundles**\n\nLogo + Gif ➜ 7€\nDrawing + Thumbnail ➜ 8€\n\n<a:955148947423379526:995764754129887253>**・Payment Methods**\n\n**PayPal**, **Nitro**, are the only accepted methods\n\n <:987612992294252615:995745604842033152> - OPEN A TICKET IN <#986311016755384320> AND BUY NOW! -  <:si:986356619602448464>")
     .setThumbnail(guild.iconURL())
     .setColor("#034efc")
     .setTimestamp()
