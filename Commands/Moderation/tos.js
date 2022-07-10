31db0c775298080ddcf77c9eec3a0dc566cc7958
ImDavide <98126828+ImDavide@users.noreply.github.com>
2022-07-10 20:53:03 +0200

    Update tos.js

--- Commands/Moderation/tos.js
+++ Commands/Moderation/tos.js
@@ -11,14 +11,14 @@ module.exports = {
         const { guild, member, user } = interaction
         const Embed = new MessageEmbed()
                            
-        .setAuthor(guild.name, guild.iconURL())
-    .setTitle("__DS STudios - TOS__")
+    .setAuthor(guild.name, guild.iconURL())
+    .setTitle("__DS STUDIOS TOS__")
     .setDescription('• Follow Discord\'s TOS.\n• No racist or discriminatory nicknames.\n• No NSFW in profile pictures or in any channel.\n• No harassment or hate speech towards others.\n• No spamming text/images.\n• Do not ping any staff unless its important.\n• Criticism is fine, but pure hate isn\'t.\n• Do not beg for any free stuff, you wont get it.\n• No advertising without our permission at all.\n• We are not responsible for any bans.\n• Be patient and respectful.')
-    .setColor("#0066FF")
+    .setColor("#034efc")
     .setTimestamp()
     .setFooter(guild.name, guild.iconURL())
 
-     interaction.channel.send({ embeds: [Embed]})
+     interaction.channel.send({content: "||@everyone||", embeds: [Embed]})
      interaction.reply({ content: "✅ | Operation completed successfully.", ephemeral: true })
     }
 }
