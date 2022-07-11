baf0d1b1c617d8bb10a9d2bdbbf8c1eb9360ca8f
ImDavide <98126828+ImDavide@users.noreply.github.com>
2022-07-11 23:02:33 +0200

    Update userUpdate.js

--- Events/Guild/userUpdate.js
+++ Events/Guild/userUpdate.js
@@ -12,7 +12,7 @@ module.exports = {
 async execute(oldUser, newUser, client) {
     // console.log(oldUser, newUser, client)
 
-const log = ("995379652388466708");
+const log = ("995767189258907838");
 const embed1 = new MessageEmbed()
 
   .setAuthor("DS Studios", "https://cdn.discordapp.com/attachments/989230929362976828/990400243101233152/IMG-20220625-WA0017.jpg")
